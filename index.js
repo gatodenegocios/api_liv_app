1c2a1d422f4a29a9b7aff62a6c3fedaaa5aab7a4
Moises <moisesalonso01@gmail.com>
2020-08-08 17:57:43 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-console.log("AAAAAAAAA");
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const app = express();
+const crypto = require('crypto');
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
 
 app.listen(3000,
 	() => console.log('Server On')
 );
+
+app.get('/',(req,res) => {
+	console.log("yaho");
+	res.send('pe');
+});
