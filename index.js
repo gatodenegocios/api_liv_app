3dafb9c71dd2623bb5c901a2eca2e2010d6fb47c
moises <moisesalonso01@gmail.com>
2020-08-10 04:17:17 +0000

    login e cadastro funcionando

--- index.js
+++ index.js
@@ -1,19 +1,237 @@
+require('dotenv').config();
+
 const express = require('express');
-const session = require('express-session');
+//const session = require('express-session');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const app = express();
+const mysql = require('mysql');
 const crypto = require('crypto');
+
+
+const { v4: uuidv4 } = require('uuid');
+
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'senha',
+  database : 'liv_app',
+  charset  : 'utf8mb4'
+});
+
+connection.connect(function(err){
+  if(err) return console.log(err);
+  console.log('conectou!');
+});
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine','ejs');
 
+/*
+
+  DROP DATABASE liv_app;
+  CREATE DATABASE liv_app;
+
+  USE liv_app;
+
+  CREATE TABLE People(
+    idPeople VARCHAR(36) NOT NULL PRIMARY KEY,
+    user VARCHAR(64) NOT NULL UNIQUE,
+    hash VARCHAR(128) NOT NULL UNIQUE,
+    salt VARCHAR(16) NOT NULL UNIQUE,
+    value DOUBLE NOT NULL DEFAULT 1000
+  ) ENGINE=InnoDB CHARACTER SET utf8;
+
+  CREATE TABLE Transfer(
+    idTransfer VARCHAR(36) NOT NULL PRIMARY KEY,
+    idPeopleFrom VARCHAR(36) NOT NULL,
+    idPeopleTo VARCHAR(36) NOT NULL,
+    value DOUBLE NOT NULL,
+
+    FOREIGN KEY (idPeopleFrom) REFERENCES People (idPeople),
+    FOREIGN KEY (idPeopleTo) REFERENCES People (idPeople)
+  ) ENGINE=InnoDB CHARACTER SET utf8;
+
+*/
+
+
+/**
+ * PASSWORDREGION 
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+let genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
+
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+let sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+function saltHashPassword(userpassword) {
+    var salt = genRandomString(16); /** Gives us salt of length 16 */
+    var passwordData = sha512(userpassword, salt);
+    return passwordData;
+}
+
+
+
+
 app.listen(3000,
-	() => console.log('Server On')
+  () => console.log('Server On')
 );
 
-app.get('/',(req,res) => {
-	console.log("yaho");
-	res.send('pe');
+app.get('/', (req, res) => {
+  res.send('Funcionando!');
 });
+
+app.post('/sign-up', (req, res) => {
+
+  let passwordData = saltHashPassword(req.body.password);
+
+  let _id = uuidv4();
+  let _user = req.body.user;
+  let _hash = passwordData.passwordHash;
+  let _salt = passwordData.salt;
+
+  connection.query("SELECT 1 FROM People WHERE People.user = ? LIMIT 1;",[
+    _user
+    ],  function(error, results){
+        if(error) {
+          console.log(error);
+          return res.status(500).json({
+            success: false,
+            msg: "Erro de conexão com o banco."
+          });
+        }
+        if(results.length>=1){
+          return res.status(406).json({
+            success: false,
+            msg: "Esse usuario já existe!"
+          });
+        }else{console.log("ARIU");
+        
+          connection.query("INSERT INTO People VALUES(?,?,?,?,DEFAULT);",[
+                _id,
+                _user,
+                _hash,
+                _salt
+              ],
+              function(error, results){ 
+                if(error) {console.log(_hash.length);console.log(_salt.length);
+                  console.log(error);
+                  return res.status(500).json({
+                    success: false,
+                    msg: "Erro de conexão com o banco."
+                  });
+                }else{
+                  console.log("Usuario criado!");
+                  return res.status(201).json( {
+                    success: false,
+                    msg: "Usuario criado!"
+                  });
+                }
+          });
+      }
+  });
+ // res.send('success');
+});
+
+app.post('/sign-in', (req, res) => {
+  let passwordData = saltHashPassword(req.body.password);
+
+  let _user = req.body.user;
+  let _password = req.body.password;
+
+
+  connection.query("SELECT * FROM People WHERE People.user = ? LIMIT 1;",[
+    _user
+    ],
+    function(error, results){
+      if(error) { 
+        console.log(error);
+        return res.status(500).json({
+          success: false,
+          msg: "Erro de conexão com o banco."
+        });
+      }
+
+      if(results.length <= 0){
+        console.log("Usuario não existe");
+        return res.status(406).json({
+          success: false,
+          msg: "Esse usuario já existe!"
+        });
+      }else{
+
+        let _salt  = results[0].salt;
+        let _hashUser = (sha512(_password,_salt)).passwordHash;
+
+        if(_hashUser === results[0].hash){ console.log("logado");
+
+          const user = {
+            user: results[0].user,
+            value : results[0].value
+          };
+
+
+          const acessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+
+          res.status(201).json({
+            success: true,
+            msg: "success",
+            token: acessToken,
+          });
+
+        }else{
+          console.log("Senha errada");
+          return res.status(406).json({
+            success: false,
+            msg: "Senha errada!"
+          });
+        }
+      }
+    }
+
+  );
+
+
+});
+
+
+
+function authenticateToken(req,res,next){
+  const _authHeader = req.headers['authorization'];
+  const _token = _authHeader && _authHeader.split(' ')[1];
+
+  if (_token == null){ 
+    console.log("erro 401"); 
+  return res.sendStatus(401);
+}
+
+  jwt.verify(_token,process.env.ACCESS_TOKEN_SECRET, (err,user) => {
+    if (err){ console.log("erro 403"); return res.sendStatus(403);}
+
+    req.user = user;
+    next();
+  });
+}
